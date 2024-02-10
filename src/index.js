df60b54ac6efa05aacfe792b16455c3fa1f15968
Taba <elena.tabakov@gmail.com>
2024-02-10 23:24:23 +0200

    User profile/ Chat

--- src/index.js
+++ src/index.js
@@ -1,5 +1,27 @@
-import './scss/styles.scss'
+import './scss/styles.scss';
 
+const googleStyleForm = () => {
+  const fields = document.querySelectorAll('.input_wrapper:not(.flex-row) input');
 
+  for (const field of fields) {
+      field.addEventListener('blur', (event) => {
+          const sel = event.target;
+          const parent = sel.parentNode;
+          if (sel.value) {
+              sel.classList.add('filled__input');
+              parent.classList.add('filled');
+          } else {
+              sel.classList.remove('filled__input');
+              parent.classList.remove('filled');
+          }
+      });
+  }
+}
 
+document.addEventListener('DOMContentLoaded', (event) => {
+  let elementExists = document.querySelector('.form__google-style');
 
+  if(elementExists) 
+      googleStyleForm();
+
+});
