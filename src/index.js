3dc12860c697f54f00d97c23301ccfdbd3406b0f
Taba <elena.tabakov@gmail.com>
2024-02-11 21:51:33 +0200

    add newline on the chat.html

--- src/index.js
+++ src/index.js
@@ -1,27 +1,27 @@
 import './scss/styles.scss';
 
 const googleStyleForm = () => {
-  const fields = document.querySelectorAll('.input_wrapper:not(.flex-row) input');
+    const fields = document.querySelectorAll('.input_wrapper:not(.flex-row) input');
 
-  for (const field of fields) {
-      field.addEventListener('blur', (event) => {
-          const sel = event.target;
-          const parent = sel.parentNode;
-          if (sel.value) {
-              sel.classList.add('filled__input');
-              parent.classList.add('filled');
-          } else {
-              sel.classList.remove('filled__input');
-              parent.classList.remove('filled');
-          }
-      });
-  }
+    for (const field of fields) {
+        field.addEventListener('blur', (event) => {
+            const sel = event.target;
+            const parent = sel.parentNode;
+            if (sel.value) {
+                sel.classList.add('filled__input');
+                parent.classList.add('filled');
+            } else {
+                sel.classList.remove('filled__input');
+                parent.classList.remove('filled');
+            }
+        });
+    }
 }
 
 document.addEventListener('DOMContentLoaded', (event) => {
-  let elementExists = document.querySelector('.form__google-style');
+    let elementExists = document.querySelector('.form__google-style');
 
-  if(elementExists) 
-      googleStyleForm();
+    if (elementExists)
+        googleStyleForm();
 
 });
